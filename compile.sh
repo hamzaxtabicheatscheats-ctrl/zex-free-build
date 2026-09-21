76d8e3242d06f8f032ddf82dd031e3c03a87c83c
ZEXTABI <randtabiiat@gmail.com>
2026-09-22 02:03:07 +0500

    Fix compile.sh explicit source file list for clean build

--- compile.sh
+++ compile.sh
@@ -8,30 +8,40 @@ OBJS=()
 
 compile_file() {
     local src="$1"
+    if [ ! -f "$src" ]; then
+        echo "Skip missing: $src"
+        return
+    fi
     local obj="${src//\//_}.o"
     echo "Compiling $src..."
     clang -arch arm64 -isysroot "$SDK" -miphoneos-version-min=15.0 -fobjc-arc -Wno-everything -I. -I./kexploit -I./compat -I./XPF/src -I./XPF/external/ChOma/include -c "$src" -o "$obj"
     OBJS+=("$obj")
 }
 
-# Compile main C/ObjC files
-for f in *.m *.c; do
-    [ -f "$f" ] && compile_file "$f"
+# Compile explicit source list
+SOURCES=(
+    "main.m"
+    "AppDelegate.m"
+    "ZEXInjectorVC.m"
+    "ZEXFileService.m"
+    "MCMBridge.m"
+    "MCMFilzaIntegration.m"
+    "sandbox_escape.m"
+    "apfs_own.m"
+    "kexploit/kexploit_opa334.m"
+    "kexploit/krw.m"
+    "kexploit/kutils.m"
+    "kexploit/offsets.m"
+    "kexploit/vnode.m"
+    "kpf/patchfinder.m"
+    "utils/file.c"
+    "utils/hexdump.c"
+    "utils/process.c"
+)
+
+for f in "${SOURCES[@]}"; do
+    compile_file "$f"
 done
-
-# Compile kexploit directory files
-if [ -d "kexploit" ]; then
-    for f in kexploit/*.m kexploit/*.c; do
-        [ -f "$f" ] && compile_file "$f"
-    done
-fi
-
-# Compile XPF directory files if present
-if [ -d "XPF/src" ]; then
-    for f in XPF/src/*.c XPF/src/*.m; do
-        [ -f "$f" ] && compile_file "$f"
-    done
-fi
 
 echo "=== Linking Binary ==="
 clang -arch arm64 -isysroot "$SDK" -miphoneos-version-min=15.0 \
